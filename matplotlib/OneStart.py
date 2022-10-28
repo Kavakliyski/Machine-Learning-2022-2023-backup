c15d499789ecfa9a90b762a28edb2fc86af134b6
luskoeqk <77803201+luskoeqk@users.noreply.github.com>
2022-10-28 15:24:35 +0300

    Update OneStart.py

--- matplotlib/OneStart.py
+++ matplotlib/OneStart.py
@@ -1,20 +1,31 @@
 from matplotlib import pyplot as plt
 
-# Median Developer Salaries by Age (for all languages)
-ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
-dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
+plt.style.use('ggplot')
 
+ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+
+
+# Median Developer Salaries by Age (for all languages)
+dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
 plt.plot(ages_x, dev_y, color='k', linestyle='--', marker='.', label='All Devs')
 
 
 # Median Python Developer Salaries by Age
 py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640]
+plt.plot(ages_x, py_dev_y, 'b', linestyle='--', marker='.', label='Python Devs')
 
-plt.plot(ages_x, py_dev_y, 'b-.', marker='.', label='Python Devs')
+
+# Median JavaScript Developer Salaries by Age
+js_dev_y = [37810, 43515, 46823, 49293, 53437, 56373, 62375, 66674, 68745, 68746, 74583]
+plt.plot(ages_x, js_dev_y, 'y', linestyle='--', marker=".", label='JavaScript Devs')
+
 
 plt.xlabel('Ages')
 plt.ylabel('Median Salary (USD)')
 plt.title('Median Salary (USD) by age')
 
-plt.legend(['A', 'B'])
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+
 plt.show()
